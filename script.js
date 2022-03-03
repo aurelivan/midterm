c031d7b4d9e6e90ac06c476f9e1e63887d794338
unknown <Aureliusivanwijaya@gmail.com>
2022-03-03 13:16:59 +0700

    page2

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+//Page 1
+
+//Algorithm for change gender
 var x = document.getElementById("homechar1");
 x.style.display = "block";
 var y = document.getElementById("homechar2");
@@ -33,3 +36,117 @@ function chooseChara(){
 
 
 var x = document.getElementById("")
+
+
+
+//Page 2
+
+//Algoritm for time
+var d,h,m,s,animate;
+
+function init(){
+    d=new Date();
+    m= 23;
+    s= 00;
+    clock();
+};
+
+function clock(){
+    s++;
+    if(s==60){
+        s=0;
+        m++;
+        if(m==24){
+            m=0;
+        }
+    }
+    $('min',s);
+    $('hr',m);
+    animate=setTimeout(clock,1000);
+};
+
+function $(id,val){
+    if(val<10){
+        val='0'+val;
+    }
+    document.getElementById(id).innerHTML=val;
+};
+
+window.onload=init;
+
+//Algorithm for status bar
+
+var i = 0;
+var width_makan = 100;
+var width_tidur = 100;
+var width_belajar = 100;
+var width_bermain = 100;
+move();
+function move() {
+  if (i == 0) {
+    i = 1;
+    var makan = document.getElementById("proggress_bar1");
+    var tidur = document.getElementById("proggress_bar2");
+    var bermain = document.getElementById("proggress_bar3");
+    var belajar = document.getElementById("proggress_bar4");
+    var id = setInterval(frame, 50);
+    function frame() {
+        if(width_makan >0){
+        width_makan = width_makan - 0.1;
+        makan.style.width = width_makan + "%";
+        }
+        if(width_tidur >0){
+        width_tidur = width_tidur - 0.1;
+        tidur.style.width = width_tidur + "%";
+        }
+        if(width_bermain >0){
+        width_bermain = width_bermain - 0.1;
+        bermain.style.width = width_bermain + "%";
+        }
+        if(width_belajar >0){
+        width_belajar = width_belajar - 0.1;
+        belajar.style.width = width_belajar + "%";
+        }
+      }
+    }
+  }
+
+  function tambah_makan(){
+      for(var i = 0; i < 20 ; i++){
+      if(width_makan > 100){
+          return;
+      }
+      width_makan = width_makan +1
+    }
+}
+
+function tambah_belajar(){
+    for(var i = 0; i < 20 ; i++){
+    if(width_belajar > 100){
+        return;
+    }
+    width_belajar = width_belajar +1
+  }
+}
+
+
+function tambah_tidur(){
+    for(var i = 0; i < 20 ; i++){
+    if(width_tidur > 100){
+        return;
+    }
+    width_tidur = width_tidur +1
+  }
+}
+
+
+function tambah_bermain(){
+    for(var i = 0; i < 20 ; i++){
+    if(width_bermain > 100){
+        return;
+    }
+    width_bermain = width_bermain +1
+  }
+}
+
+
